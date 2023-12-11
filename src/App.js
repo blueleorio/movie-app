a6b591ab4ae5f9cd3e09fb407a573e4bef9021a3
BeluKotu <vietthanh456@gmail.com>
2023-12-11 18:46:50 +0700

    MinorUpdate

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const API = {
+  // Example: https://api.themoviedb.org/3/movie/550?api_key=04db60b988f9f9b15d783d86a593a392
   key: "04db60b988f9f9b15d783d86a593a392",
+  base: "https://api.themoviedb.org/3/",
   token:
     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNGRiNjBiOTg4ZjlmOWIxNWQ3ODNkODZhNTkzYTM5MiIsInN1YiI6IjY1NzZkZDU2NGJmYTU0MDBjNDA5YzEzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LczGGFStgjMCazXyYf0fo32UtLXgCo29mfESeSxoQ5M",
 };
 
+// TODO:  Components, what and what not
 // API Document: https://developer.themoviedb.org/reference/intro/getting-started
 // GITHUB Repo: https://github.com/blueleorio/movie-app
 
@@ -29,6 +32,8 @@ function App() {
       setLoading(true);
       // Process
       try {
+        // Example: https://api.themoviedb.org/3/movie/550?api_key=04db60b988f9f9b15d783d86a593a392
+
         const url = `${API.base}weather?q=${searchCity}&units=metric&APPID=${API.key}`;
         const res = await fetch(url);
         const data = await res.json();
