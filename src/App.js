c43e3a79eb1338825230cf4a338cd719306b03b5
BeluKotu <vietthanh456@gmail.com>
2023-12-11 17:42:19 +0700

    Barebone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const API = {
+  key: "04db60b988f9f9b15d783d86a593a392",
+  token:
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNGRiNjBiOTg4ZjlmOWIxNWQ3ODNkODZhNTkzYTM5MiIsInN1YiI6IjY1NzZkZDU2NGJmYTU0MDBjNDA5YzEzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LczGGFStgjMCazXyYf0fo32UtLXgCo29mfESeSxoQ5M",
+};
+
+// API Document: https://developer.themoviedb.org/reference/intro/getting-started
+// GITHUB Repo: https://github.com/blueleorio/movie-app
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchCity(searchInput);
+  };
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      if (!searchCity) return;
+      setLoading(true);
+      // Process
+      try {
+        const url = `${API.base}weather?q=${searchCity}&units=metric&APPID=${API.key}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        if (res.ok) {
+          setWeatherInfo(
+            `${data.name}. ${data.sys.country} ${data.weather[0].description}, ${data.main.temp}`
+          );
+          setErrorMessage("");
+        } else {
+          setErrorMessage(data.message);
+        }
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+      setLoading(false);
+    };
+    fetchWeatherData();
+  }, [searchCity]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="City"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <button>Search</button>
+      </form>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <div>{weatherInfo}</div>
+          <div style={{ color: "red" }}> {errorMessage}</div>
+        </>
+      )}
+    </>
   );
 }
 
