19bf31c2c5a4ae370063e42d0e87d41a70f749f1
BeluKotu <vietthanh456@gmail.com>
2024-01-16 04:11:20 +0700

    HelloWorld

--- src/App.js
+++ src/App.js
@@ -14,64 +14,9 @@ const API = {
 // GITHUB Repo: https://github.com/blueleorio/movie-app
 
 function App() {
-  const [searchInput, setSearchInput] = useState("");
-  const [searchCity, setSearchCity] = useState("");
-  const [weatherInfo, setWeatherInfo] = useState("");
-
-  const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setSearchCity(searchInput);
-  };
-
-  useEffect(() => {
-    const fetchWeatherData = async () => {
-      if (!searchCity) return;
-      setLoading(true);
-      // Process
-      try {
-        // Example: https://api.themoviedb.org/3/movie/550?api_key=04db60b988f9f9b15d783d86a593a392
-
-        const url = `${API.base}weather?q=${searchCity}&units=metric&APPID=${API.key}`;
-        const res = await fetch(url);
-        const data = await res.json();
-        if (res.ok) {
-          setWeatherInfo(
-            `${data.name}. ${data.sys.country} ${data.weather[0].description}, ${data.main.temp}`
-          );
-          setErrorMessage("");
-        } else {
-          setErrorMessage(data.message);
-        }
-      } catch (error) {
-        setErrorMessage(error.message);
-      }
-      setLoading(false);
-    };
-    fetchWeatherData();
-  }, [searchCity]);
-
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="City"
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-        />
-        <button>Search</button>
-      </form>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <div>{weatherInfo}</div>
-          <div style={{ color: "red" }}> {errorMessage}</div>
-        </>
-      )}
+      <p>Hello World!</p>
     </>
   );
 }
