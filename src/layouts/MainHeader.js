4cf287babc87a782228ed775c3a2b563830789fa
BeluKotu <vietthanh456@gmail.com>
2024-01-29 23:33:22 +0700

    clean

--- src/layouts/MainHeader.js
+++ src/layouts/MainHeader.js
@@ -4,59 +4,33 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
+
 import GitHubIcon from "@mui/icons-material/GitHub";
 import Switch from "@mui/material/Switch";
 
 import { ThemeContext } from "../contexts/ThemeProvider";
-import { useAuth } from "../contexts/AuthContext";
-import { Link, useLocation } from "react-router-dom";
 
-const pages = ["Products", "Pricing", "Blog"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+import { Link } from "react-router-dom";
 
 function MainHeader() {
-  let location = useLocation();
-  let auth = useAuth();
-
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
   const { isDarkTheme, toggleTheme } = React.useContext(ThemeContext);
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-  const handleLogout = () => {
-    // handleMenuClose(); //menu close before signout so that login won't pop up.
-    auth.signout();
-  };
 
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <GitHubIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Link
+            to="/page/1"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <GitHubIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          </Link>
           <Typography
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/page/1"
             sx={{
               mr: 2,
               display: { xs: "none", md: "flex" },
@@ -70,48 +44,17 @@ function MainHeader() {
             ムービー
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <GitHubIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Link
+            to="/page/1"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <GitHubIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          </Link>
           <Typography
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/page/1"
             sx={{
               mr: 2,
               display: { xs: "flex", md: "none" },
@@ -125,56 +68,17 @@ function MainHeader() {
           >
             ムービー
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+
           {/* Switch Theme toggle */}
           <Box sx={{ flexGrow: 0 }}>
             <IconButton onClick={toggleTheme}>
               <Switch checked={isDarkTheme} />
             </IconButton>
           </Box>
-          {/* User Logo Icon */}
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
         </Toolbar>
       </Container>
     </AppBar>
   );
 }
+
 export default MainHeader;
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,31 +1,14 @@
 import * as React from "react";
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import { HomePage } from "../pages/HomePage";
-import LogInModal from "../components/LogInModal";
 import MovieDetail from "../pages/MovieDetail";
-// import FavoritePage from "../pages/Favorite";
+
 import NoMatch from "../pages/NoMatch";
-import { useAuth } from "../contexts/AuthContext";
-// import LogInForm from "../components/LogInForm";
-import MovieDetails from "../pages/Test";
 
 function Router() {
   let location = useLocation();
-  let state = location.state;
-  function RequireAuth({ children }) {
-    let auth = useAuth();
-    console.log("user status:", auth.user);
-    if (!auth.user) {
-      // Redirect them to the /login page, but save the current location they were
-      // trying to go to when they were redirected. This allows us to send them
-      // along to that page after they login, which is a nicer user experience
-      // than dropping them off on the home page.
 
-      return <Navigate to="/form" state={{ from: location }} replace />;
-    }
-    return children;
-  }
   return (
     <>
       <Routes location={location}>
@@ -33,23 +16,9 @@ function Router() {
           <Route index element={<HomePage />} />
           <Route path="/page/:pageId" element={<HomePage />} />
           <Route path="/movie/:movieId" element={<MovieDetail />} />
-          <Route path="/form" element={<LogInModal />} />
           <Route path="*" element={<NoMatch />} />
-          {/* <Route
-            path="/favorite"
-            element={
-              <RequireAuth>
-                <FavoritePage />
-              </RequireAuth>
-            }
-          /> */}
         </Route>
       </Routes>
-      {state?.backgroundLocation && (
-        <Routes>
-          <Route path="/form" element={<LogInModal />} />
-        </Routes>
-      )}
     </>
   );
 }
